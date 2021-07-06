bbd4ceeda8d359b264174ee46f1f29c307a8742d
Davide Domini <davide.domini@studio.unibo.it>
2021-07-06 09:15:35 +0200

    aggiunto pulsante vendi moto

--- app/src/main/java/com/example/e_moto/SellNewMotoFragment.java
+++ app/src/main/java/com/example/e_moto/SellNewMotoFragment.java
@@ -7,14 +7,22 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.material.textfield.TextInputEditText;
+
 public class SellNewMotoFragment extends Fragment {
 
+
+    private TextInputEditText modello;
+    private TextInputEditText descrizione;
+    private TextInputEditText prezzo;
+    private Button vendi;
 
 
     @Nullable
@@ -30,6 +38,19 @@ public class SellNewMotoFragment extends Fragment {
         Activity activity = getActivity();
 
         if(activity != null){
+
+            this.modello = view.findViewById(R.id.modello_add);
+            this.descrizione = view.findViewById(R.id.descrizione_add);
+            this.prezzo = view.findViewById(R.id.prezzo_add);
+            this.vendi = view.findViewById(R.id.sell_moto);
+
+            this.vendi.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    //Aggiungo moto al realtime db
+                    
+                }
+            });
 
         }
     }
